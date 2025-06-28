87f0e90f87e760a422f7c876d009e506acaeac46
QuantumHrishi <100389358+QuantumHrishi@users.noreply.github.com>
2025-06-28 12:31:35 -0400

    ctof

---
+++ Temperature.java
@@ -0,0 +1,9 @@
+public class Temperature {
+    public static void main(String[] args){
+        celsiusToFahrenheit(0);
+    }
+    public double celsiusToFahrenheit(double celsius) {
+            return (celsius * 9.0 / 5.0) + 32.0;
+    }
+
+}
