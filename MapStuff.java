43b3fa5ef799eb737fed4968425f39c85ba2deeb
QuantumHrishi <100389358+QuantumHrishi@users.noreply.github.com>
2025-08-05 00:42:45 +0000

    maps

---
+++ MapStuff.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStuff {
+    public static void main(String[] args) {
+        Map<Integer, String> test = new HashMap<>();
+        test.put(3, "1");
+        test.put(2, "5");
+        test.put(4, "9");
+        test.put(1, "7");
+        System.out.println(test);
+        
+    }
+    
+    
+}
